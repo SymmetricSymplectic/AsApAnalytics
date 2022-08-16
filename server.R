2c0498d64200b9ddf90d87bcb4e356e855a906b0
Korvalyov <felixcm@ciencias.unam.mx>
2022-08-16 10:43:39 -0500

    is na removed creating issues

--- server.R
+++ server.R
@@ -564,7 +564,7 @@ server <- function(input, output, session){
   merged_data<-eventReactive(input$update,{
     data2 <-datasetInput()
     data1 <-df_products_upload()
-    data1[is.na(data1)] <- 0
+    #data1[is.na(data1)] <- 0
     data3 <-priceInput()
     if (is.null(data1)){
       if (is.null(data3))
